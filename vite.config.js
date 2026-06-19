5a5b2d2d2964e0a1d8e3e0f11234e7094c8eb8fe
Dan Fortinho <fortinhodanvhamba@gmail.com>
2026-06-19 16:39:26 +0200

    mise à jour vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // CONFIG PROPRE POUR PRODUCTION (Render / Vercel / Netlify)
 export default defineConfig({
   plugins: [react()],
-
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
